451faaf5c1e7ae4ecf59710903f9d898dda92563
Divya prakash tripathi <divytripathidivy@gmail.com>
2024-04-21 14:54:22 +0530

    c5

--- api/index.js
+++ api/index.js
@@ -2,10 +2,15 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
+import path from 'path';
+
+
+
+
+const __dirname=path.resolve();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -14,31 +19,47 @@ const io = new Server(server, {
   },
 });
 
+
+
+
+
 server.listen(3001, () => {
   console.log("SERVER IS RUNNING");
 });
 
-
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
 
 io.on("connection", (socket) => {
   console.log("client1 connected");
 
   socket.on("receiverFormSubmitted", (data) => {
     console.log("Received message:", data);
+
     socket.broadcast.emit("sendMessageToClient2", data.message);
     console.log("Sent message to sendMessageToReceiverPost");
   });
 });
 
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
-});
 
-app.use("/api", (req, res, next) => {
-  res.send("aur bhai");
-});
 
-// app.use((req, res, next) => {
-//   res.send("server connected and running");
-// });
+app.use("/api",(req, res, next) => {
+ res.send("aur bhai")
+  });
+
+  
+    
+  app.use(express.static(path.join(__dirname, '/client/dist')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  });
+
+
+
+  app.use((req, res, next) => {
+    res.send("server conneted and running hiiiiiiiiiiiiiiii")
+     });
+ 
