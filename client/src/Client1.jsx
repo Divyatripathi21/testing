9671dbc0abb655404edb90c97357ac76368e7bbb
Divya prakash tripathi <divytripathidivy@gmail.com>
2024-04-21 15:40:24 +0530

    c6

--- client/src/Client1.jsx
+++ client/src/Client1.jsx
@@ -3,7 +3,9 @@ import io from "socket.io-client";
 
 export default function Client1() {
   const handleClient1 = () => {
+    console.log("1");
     const socket = io.connect("https://testing-ftvh.onrender.com/");
+    console.log("2");
     socket.on("connect", () => {
       console.log("Connected to server");
       socket.emit("receiverFormSubmitted", { message: "yes" });
